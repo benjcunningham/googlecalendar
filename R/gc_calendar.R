d21c02b048fdf5441e436246f1d3295b68c6c9d0
Ben Cunningham <benjamescunningham@gmail.com>
2016-05-27 14:15:15 -0500

    Update documentation to match 35dabff

--- R/gc_calendar.R
+++ R/gc_calendar.R
@@ -5,45 +5,11 @@
 #' available through the Google Calendar API \code{CalendarList}
 #' resource. Use requires authorization.
 #'
-#' A \code{googlecalendar} object will, in cases where the the field is
-#' non-\code{NULL}, contain the following information:
-#'
-#' \itemize{
-#'   \item \code{accessRole} Character string representing the effective
-#'     access role that the authenticated user has on the calendar.
-#'     Possible values are: \code{freeBusyReader}, \code{reader},
-#'     \code{writer}, and \code{owner}.
-#'   \item \code{backgroundColor} Character string representing the main
-#'     color of the calendar in the hexadecimal format \code{#0088aa}.
-#'   \item \code{colorId} Character string representing the color of the
-#'     calendar using an index-based ID.
-#'   \item \code{deleted} Logical indicating whether the calendar has
-#'     been deleted from the \code{CalendarList} resource.
-#'   \item \code{description} Character string representing the
-#'     user-specified description of the calendar.
-#'   \item \code{etag} Character string representing the ETag of the
-#'     calendar listing in the \code{CalendarList} resource.
-#'   \item \code{foregroundColor} Character string representing the
-#'     foreground color of the calendar in the hexadecimal format
-#'     \code{#ffffff}.
-#'   \item \code{hidden} Logical indicating whether the calendar has
-#'     been hidden from the list.
-#'   \item \code{id} Character string representing the unique identifier
-#'     of the calendar.
-#'   \item \code{location} Character string representing the geographic
-#'     location of the calendar as free-form text.
-#'   \item \code{primary} Logical indicating whether the calendar is the
-#'     primary calendar of the authenticated user.
-#'   \item \code{selected} Logical indicating whether the calendar
-#'     content has been selected to show up in the browser-based
-#'     calendar UI.
-#'   \item \code{summary} Character string representing the title of the
-#'     calendar.
-#'   \item \code{summaryOverride} Character string representing the
-#'     title that the authenticated user has set for the calendar.
-#'   \item \code{timeZone} Character string representing the time zone
-#'     of the calendar.
-#' }
+#' For more information on the structure of the \code{CalendarList}
+#' resource (and therefore the expected contents of a
+#' \code{googlecalendar} object), see the Google Calendar API
+#' \href{https://developers.google.com/google-apps/calendar/v3/reference/calendarList}{CalendarList
+#' Overview}.
 #'
 #' @name googlecalendars
 #'
@@ -58,9 +24,8 @@
 #' @param ... Optional arguments to be passed to \code{\link{grep}}.
 #' @template verbose
 #'
-#' @return A single-row \code{googlecalendar} object (a custom class
-#'   wrapping \code{\link[dplyr]{tbl_df}}) representing metadata for a
-#'   single calendar.
+#' @return A \code{googlecalendar} object (a custom class wrapping a
+#'   named list representing metadata for a single calendar.
 #'
 #' @examples
 #' \dontrun{
