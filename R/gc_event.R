522345550ead7deb851a990712ab9bb3ace5a08b
Ben Cunningham <benjamescunningham@gmail.com>
2016-05-29 20:36:24 -0500

    Documentation to match c89f208

--- R/gc_event.R
+++ R/gc_event.R
@@ -1,3 +1,52 @@
+#' Retrieve a calendar event
+#'
+#' Retrieves metadata for a Google Calendar event in the form of an
+#' \code{event} object. This function requests information made
+#' available through the Google Calendar API \code{Events} resource. Use
+#' requires authorization.
+#'
+#' The \code{gc_event_query} method can accept an arbitrary number of
+#' named arguments via the \dots parameter. These values are passed as
+#' part of the HTTP request to the \code{Events} resource; queries may
+#' include those parameters listed in the
+#' \href{https://developers.google.com/google-apps/calendar/v3/reference/events/list#request}{Events: list}
+#' reference documentation. Notable parameters include:
+#'
+#' \itemize{
+#'   \item{q} Free text search terms to find in any field.
+#'   \item{singleEvents} Logical indicating whether to expand recurring
+#'     events into instances and only return single one-off events.
+#'   \item{timeMax} Exclusive upper bound of the start time filter
+#'     (formatted as an RFC3339 timestamp).
+#'   \item{timeMin} Inclusive lower bound of the end time filter
+#'     (formatted as an RFC3339 timestamp).
+#' }
+#'
+#' For more information on the structure of the \code{Events} resource
+#' (and therefore the expected contents of an \code{events} object), see
+#' the Google Calendar API
+#' \href{https://developers.google.com/google-apps/calendar/v3/reference/events}{Events
+#' Overview}.
+#'
+#' @name events
+#'
+#' @param x \code{googlecalendar} object representing the calendar for
+#'   which to list events.
+#' @param id Character string representing the event ID.
+#' @template verbose
+#' @param \dots Additional parameters to be passed as part of the HTTP
+#'   request to the API. More information on these named arguments are
+#'   available below.
+#'
+#' @examples
+#' \dontrun{
+#' cal <- gc_summary("Commitments")
+#'
+#' gc_event_id(cal, "<ID>")
+#' gc_event_query(cal, q = "Lunch with Will")
+#' }
+#'
+#' @export
 gc_event_query <- function(x, ..., verbose = TRUE) {
 
   stopifnot(methods::is(x, "googlecalendar"))
@@ -19,6 +68,8 @@ gc_event_query <- function(x, ..., verbose = TRUE) {
 
 }
 
+#' @rdname events
+#' @export
 gc_event_id <- function(x, id, verbose = TRUE) {
 
   stopifnot(methods::is(x, "googlecalendar"))
