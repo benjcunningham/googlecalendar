d56efe9838a081383fda30f3a2a40faeff8847e9
Ben Cunningham <benjamescunningham@gmail.com>
2016-06-26 22:48:42 -0500

    Add tests for non-interactive token auth

--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -12,6 +12,18 @@ test_that("The token is legitimate", {
   expect_is(.cred$token, "Token2.0")
 })
 
+test_that("Non-interactive passed object token works", {
+  auth <- gc_auth(token = "googlecalendar_token.rds", verbose = FALSE)
+  expect_is(auth, "Token2.0")
+  expect_identical(auth, .cred$token)
+})
+
+test_that("Non-interactive passed file token works", {
+  auth <- gc_auth(token = .cred$token, verbose = FALSE)
+  expect_is(auth, "Token2.0")
+  expect_identical(auth, .cred$token)
+})
+
 test_that("The token has been deregistered", {
   expect_true(gc_deauth())
   expect_null(.cred$token)
